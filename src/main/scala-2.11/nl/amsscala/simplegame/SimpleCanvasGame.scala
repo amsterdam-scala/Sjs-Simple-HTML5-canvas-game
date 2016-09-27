8a6ed5b05b22d7d405f9641ab97b14287ab78782
SeriousSoftware <vdenberg+github@gmail.com>
2016-09-27 20:56:20 +0200

    feature: GameSuite covers GameState test-only

--- src/main/scala-2.11/nl/amsscala/simplegame/SimpleCanvasGame.scala
+++ src/main/scala-2.11/nl/amsscala/simplegame/SimpleCanvasGame.scala
@@ -7,7 +7,7 @@ import scala.scalajs.js.JSApp
  * Main entry point for application start
  */
 object SimpleCanvasGame extends JSApp with Game with Page {
-  type T = Long // This sets the generic used by the whole application and tests.
+  type T = Int // This sets the generic used by the whole application and tests.
 
   /**
    * Entry point of execution
